2f7c5ebe0fd290b958a52f4e33748ed41914f23c
OndrejSoukupCoretek <OndrejSoukupCoretek>
2021-08-20 12:12:38 -0400

    sensitive true

--- 01/lnux_vm-out.tf
+++ 01/lnux_vm-out.tf
@@ -20,6 +20,6 @@ output "linux_vm_admin_username" {
 output "linux_vm_admin_password" {
   description = "Administrator password for the Virtual Machine"
   value       = random_password.linux-vm-password.result
-  #sensitive   = true
+  sensitive   = true
 }
 
